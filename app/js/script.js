84896b5c1b2c10d578a9822ab2e1970e1e545bbe
Dmitrij Ch <107755094+authorskoy@users.noreply.github.com>
2022-06-26 19:26:26 +0600

    added article & footer sections, fixed bugs.

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,15 @@ const btnHamburger = document.querySelector('#btnhamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const headerMenu = document.querySelector('.header__menu');
+const fadeOut = document.querySelectorAll('.fade-out');
+console.log(fadeOut);
 
 const toggleClass = function (e, classToE) {
   e.classList.toggle(classToE);
 }
 
 btnHamburger.addEventListener('click', function () {
+  fadeOut.forEach(e => e.style.display = 'block');
   toggleClass(header, 'open');
   toggleClass(overlay, 'fade-in');
   toggleClass(overlay, 'fade-out');
